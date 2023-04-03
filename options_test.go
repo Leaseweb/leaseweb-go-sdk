cb2166ee964316b4478beb3b95a0164b912f22a2
msslr <salari.leaseweb@gmail.com>
2023-04-03 19:00:12 +0200

    directly compare the strings result with tc.expected with a standard assert.

--- options_test.go
+++ options_test.go
@@ -1,12 +1,12 @@
-package options
+package leaseweb
 
 import (
-	"net/url"
-	"reflect"
 	"testing"
+
+	"github.com/LeaseWeb/leaseweb-go-sdk/options"
 )
 
-func TestStructToURLValues(t *testing.T) {
+func TestEncode(t *testing.T) {
 	testCases := []struct {
 		name     string
 		opts     DedicatedServerListOptions
@@ -25,7 +25,7 @@ func TestStructToURLValues(t *testing.T) {
 				PrivateNetworkCapable: Bool(true),
 				PrivateNetworkEnabled: Bool(false),
 			},
-			expected: "offset=0&limit=10&ip=10.22.192.3&macAddress=AA%3ABB%3ACC%3ADD%3AEE%3AFF&site=AMS-01&privateRackId=1&reference=test-reference&privateNetworkCapable=true&privateNetworkEnabled=false",
+			expected: "ip=10.22.192.3&limit=10&macAddress=AA%3ABB%3ACC%3ADD%3AEE%3AFF&offset=0&privateNetworkCapable=true&privateNetworkEnabled=false&privateRackId=1&reference=test-reference&site=AMS-01",
 		},
 		{
 			name: "Test Case 2: Limited options",
@@ -35,19 +35,15 @@ func TestStructToURLValues(t *testing.T) {
 				MacAddress: String(""),
 				Site:       String("AMS-01"),
 			},
-			expected: "offset=0&limit=10&macAddress=&site=AMS-01",
+			expected: "limit=10&macAddress=&offset=0&site=AMS-01",
 		},
 	}
 
 	for _, tc := range testCases {
 		t.Run(tc.name, func(t *testing.T) {
-			result := structToURLValues(tc.opts)
-
-			expectedValues, _ := url.ParseQuery(tc.expected)
-			resultValues, _ := url.ParseQuery(result)
-
-			if !reflect.DeepEqual(expectedValues, resultValues) {
-				t.Errorf("Expected '%v', but got '%v'", expectedValues, resultValues)
+			result := options.Encode(tc.opts)
+			if result != tc.expected {
+				t.Errorf("Expected '%s', but got '%s'", tc.expected, result)
 			}
 		})
 	}
