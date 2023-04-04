737c0a4656ea8f65e6ce3e95021616c08e14f4c2
msslr <salari.leaseweb@gmail.com>
2023-04-04 11:21:17 +0200

    update the namings matching Go style

--- options/options.go
+++ options/options.go
@@ -6,24 +6,24 @@ import (
 	"reflect"
 )
 
-func Encode(input interface{}) string {
-	queryValues := url.Values{}
-	structType := reflect.TypeOf(input)
-	structValue := reflect.ValueOf(input)
+func Encode(opts interface{}) string {
+	v := url.Values{}
+	ot := reflect.TypeOf(opts)
+	ov := reflect.ValueOf(opts)
 
-	for i := 0; i < structType.NumField(); i++ {
-		field := structType.Field(i)
-		value := structValue.Field(i)
+	for i := 0; i < ot.NumField(); i++ {
+		otf := ot.Field(i)
+		ovf := ov.Field(i)
 
-		if !value.IsNil() {
-			queryParam := field.Tag.Get("param")
-			if queryParam == "" {
-				queryParam = field.Name
+		if !ovf.IsNil() {
+			p := otf.Tag.Get("param")
+			if p == "" {
+				p = otf.Name
 			}
-			val := value.Elem().Interface()
-			queryValues.Add(queryParam, fmt.Sprintf("%v", val))
+			val := ovf.Elem().Interface()
+			v.Add(p, fmt.Sprintf("%v", val))
 		}
 	}
 
-	return queryValues.Encode()
+	return v.Encode()
 }
